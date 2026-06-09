d31786fa8f30f5008a3d1d6c835bc75b34c5c9a2
Daniel Polinario <projetosvisual3d@gmail.com>
2026-06-09 14:50:28 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'bootfriend-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './esperando.mp4',
+  './falando.mp4'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
